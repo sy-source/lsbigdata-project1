60fae112f9701d1191556f1a193dd25af8726265
woong2 <syoon456@naver.com>
2024-07-15 14:45:23 +0900

    data update

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-a = 10
-b = 20
-print("a==b", a == b)
+import numpy as np
+import pandas as pd
 
-#테스트
+df= pd.DataFrame({'제품'    :['사과', '딸기', '수박'],
+                   '가격'    : [1800, 1500, 3000],
+                    '판매량': [24, 38, 13]})
+                    df
+#과일의 가격 평균과 판매량 평균
+sum(df['판매량']/3)
+sum(df['가격']/3)
+
+df
+
+type(df)
+type(df["name"])
+
+sum(df["english"])/4
